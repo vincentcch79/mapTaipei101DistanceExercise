20c6fa9bbced403163111039b1091cd2c6099c37
Vincent Chang <vincentcch79@gmail.com>
2016-07-04 15:45:19 +0800

    first commit

--- MapKitDistanceExercise/ViewController.swift
+++ MapKitDistanceExercise/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    let taipei101Location = CLLocation(latitude: 25.0335, longitude: 121.5641)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+  
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.delegate = self
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        
+        
+        
     }
-
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation:CLLocation = manager.location!
+        let distance = currentLocation.distanceFromLocation(taipei101Location)
+        distanceLabel.text = "\(distance)"
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
